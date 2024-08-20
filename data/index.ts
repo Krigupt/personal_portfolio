8d0bba0efaf1164f4df726b9e0f00385d02a98d8
Krigupt <123670605+Krigupt@users.noreply.github.com>
2024-08-20 00:49:59 -0700

    first commit

---
+++ data/index.ts
@@ -0,0 +1,244 @@
+
+
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I'm passionate in transforming innovative ideas into reality",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I can travel anywhere!",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building an AI Stealth Startup",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "AI-Dent",
+    des: "ML model to detect the pathologies in x-ray images",
+    img: "/dental.jpeg",
+    iconLists: [
+      "/mongo.svg",
+      "/tail.svg",
+      "/next.svg",
+      "/python.svg",
+      "/react.svg",
+    ],
+    link: "https://youtu.be/ByvVpuJVKw0",
+  },
+  {
+    id: 2,
+    title: "Baddi Coach",
+    des: "AI Badminton Coach for optimizing badminton game.",
+    img: "/baads.png",
+    iconLists: [
+      "/mongo.svg",
+      "/tail.svg",
+      "/next.svg",
+      "/react.svg",
+      "/tensorflow.svg"
+    ],
+    link: "https://drive.google.com/file/d/1usE-ITst5gdS79WZSyEO7jXP3-zFTIOB/view?usp=sharing",
+  },
+  {
+    id: 3,
+    title: "Task Manager Application",
+    des: "Application for Managing your tasks",
+    img: "task.png",
+    iconLists: ["/re.svg", "/tail.svg", "/mongo.svg", "/node.svg"],
+    link: "https://taskmanager-4sj9.onrender.com",
+  },
+  {
+    id: 4,
+    title: "Song Recommendation System",
+    des: "A song recommendation system for personalized music suggestions  ",
+    img: "music.png",
+    iconLists: [
+      "/flask.svg",
+      "/tail.svg",
+      "/python.svg",
+      "/html5.svg",
+      "/spotify.svg",
+    ],
+    link: "https://song-classifier1.onrender.com",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+// ];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+    link: "",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Development Intern - Trompar",
+    desc: "Crafted dynamic UI components, backend services, and robust  using Flask, RESTful APIs, and MySQL, while optimizing UX with Tailwind CSS and implementing serverless architecture for peak efficiency. Leveraged CI/CD for seamless, reliable updates and continuous integration.",
+    className: "md:col-span-2",
+    thumbnail: "/images.jpeg",
+    time: "July 2023 - September 2023",
+  },
+  {
+    id: 2,
+    title: "Software Developer India - Denta Kris",
+    desc: "Engineered a scalable LLM for text classification and stock data summarization, cutting stock expiration by 30% through efficient serverless architecture. Enhanced stock management with seamless integration, optimized PostgreSQL databases, and ensured robust CI/CD practices for continuous, reliable updates.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "golden.png",
+    time: "Jul 2024 - Sep 2024",
+  },
+  {
+    id: 3,
+    title: "Data Science Intern(AI/ML) - Indian Institute of Technology",
+    desc: "Developed an AI model for diagnosing facial paralysis with 85% accuracy, leveraging deep learning and image pre-processing techniques (VGG16, VGG19, MT-CNN, KNN, dlib), and managed a dataset of over 10,000 images.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/Unknown-1.jpeg",
+    time: "Jun 2023 - Sep 2023",
+  },
+  {
+    id: 5,
+    title: "Project Manager - Aggie Sports Analytics Club",
+    desc: "Developed a badminton analysis model with live pose detection and shuttle tracking, improving gameplay analytics by 20%. Optimized performance using Python, TensorFlow.js, PoseNet, and Yolo-v8.",
+    className: "md:col-span-2",
+    thumbnail: "asa.png", // Change to a relevant image path
+    time: "September 2023 - Current",
+  },
+];
+import { FaInstagram } from "react-icons/fa";
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Krigupt",
+  },
+  {
+    id: 2,
+    img: "/insta.svg", // Update this to your Instagram icon file
+    link: "https://www.instagram.com/krishnagupta6609/", //
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/krishna-gupta-a19b67233/",
+  },
+];
